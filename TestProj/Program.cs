914436ffb77e58928ccafedd502b6149dd9f560f
saoba_1107 <aprilyang41@gmail.com>
2026-04-02 17:27:19 +0900

    첫번쨰커밋

---
+++ TestProj/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> names = new List<string>();
+            names.Add("철수");
+            names.Add("영희");
+            names.Add("민수");
+            names.Add("맹구");
+            names.Add("짱구");
+
+
+
+            for(int i =0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine("==================================");
+
+            names.Remove("맹구");
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine("==================================");
+
+            names.RemoveAt(1);
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine("==================================");
+
+            /*foreach (string name in names)
+            {
+                Console.Write(name);
+            }*/
+
+        }
+    }
+}
+
